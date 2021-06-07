a0607a95604f89993bef44074a8e3b6ec6f7e496
azerpas <xskylix@hotmail.fr>
2021-06-07 14:59:07 +0200

    Coureur methods

--- src/main/java/com/coureur/Coureur.java
+++ src/main/java/com/coureur/Coureur.java
@@ -1,10 +1,29 @@
 package com.coureur;
 
 public class Coureur {
-    private int id;
-    private String nom;
-    
-    public Coureur(){
+    private final int id;
+    private final String nom;
+    private float temps;
 
+    public Coureur(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
     }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public float getTemps() {
+        return this.temps;
+    }
+
+    public void setTemps(float temps) {
+        this.temps = temps;
+    }
+    
 }
